c100b3470ec066b09eb05d476792ff12b2c232f0
Jmoney <67088606+AsanroJay@users.noreply.github.com>
2026-02-15 22:51:54 +0800

    feat: implement player pick up and drop logic

--- Assets/Avatar/Scripts/Avatar_Actions.cs
+++ Assets/Avatar/Scripts/Avatar_Actions.cs
@@ -3,11 +3,14 @@ using UnityEngine.InputSystem;
 
 public class Avatar_Move : MonoBehaviour
 {
+    [SerializeField] private GameObject objectInteractPoint;
+    [SerializeField] private float interactRange = 2f;
 
     private float moveSpeed = 7f;
-     private Vector2 moveInput;
+    private Vector2 moveInput;
 
-    // Update is called once per frame
+    private GameObject heldObject;
+
     void Update()
     {
         Vector3 move = new Vector3(moveInput.x, 0f, moveInput.y);
@@ -19,4 +22,45 @@ public class Avatar_Move : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
+    public void OnPickUp(InputValue value)
+    {
+        if (!value.isPressed) return;
+
+        if (heldObject == null)
+        {
+            TryPickUp();
+        }
+        else
+        {
+            Drop();
+        }
+    }
+
+    void TryPickUp()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, interactRange);
+
+    
+
+        foreach (Collider hit in hits)
+        {
+            if (hit.CompareTag("Food"))
+            {
+                heldObject = hit.gameObject;
+                Rigidbody rb = heldObject.GetComponent<Rigidbody>();
+                rb.isKinematic = true;
+                heldObject.transform.position = objectInteractPoint.transform.position;
+                heldObject.transform.SetParent(objectInteractPoint.transform);
+                return;
+            }
+        }
+    }
+
+    void Drop()
+    {
+        Rigidbody rb = heldObject.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        heldObject.transform.SetParent(null);
+        heldObject = null;
+    }
 }
