37495f62b3a0d94183c89c958e094a6e03696845
Jmoney <67088606+AsanroJay@users.noreply.github.com>
2026-02-18 21:13:21 +0800

    feat: implement food detection and snapping mechanic on item surfaces

---
+++ Assets/ItemSurface/Scripts/ItemSurfaceScript.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class ItemSurface : MonoBehaviour
+{
+    [SerializeField] private Transform snapPoint;
+
+    private GameObject currentItem;
+
+    private void OnTriggerStay(Collider other)
+    {
+        // if already occupide ignore
+        if (currentItem != null) return;
+
+        if (!other.CompareTag("Food")) return;
+
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        if (rb == null) return;
+
+        SnapItem(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == currentItem)
+        {
+            currentItem = null;
+        }
+    }
+
+    void SnapItem(GameObject item)
+    {
+        Rigidbody rb = item.GetComponent<Rigidbody>();
+
+        // stop all motion 
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.isKinematic = true;
+        rb.useGravity = false;
+
+     
+        item.transform.position = snapPoint.position;
+        item.transform.rotation = snapPoint.rotation;
+
+        item.transform.SetParent(snapPoint);
+
+        currentItem = item;
+    }
+
+    public void ClearSurface()
+    {
+        if (currentItem == null) return;
+
+        currentItem.transform.SetParent(null);
+        currentItem = null;
+    }
+}
